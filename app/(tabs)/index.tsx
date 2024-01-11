733835e5f469daed8fbd83aeb4a699011e815585
Senju002 <rajkaran.mei34@gmail.com>
2024-01-11 22:42:17 +0700

    Expore header with styling for search bar and filter

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Stack } from "expo-router";
+import ExploreHeader from "@/components/ExploreHeader";
+import Listing from "@/components/Listing";
 
 const Page = () => {
   return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Bookings</Link>
-      <Link href={"/listing/123"}>Listing Details Page</Link>
+    <View style={{ flex: 1 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader />,
+        }}
+      />
+      <Listing />
     </View>
   );
 };
